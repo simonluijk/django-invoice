eec47ad9820f10b7f5636f013630ad5cd2807a10
Simon Luijk <simon@luijk.co.uk>
2010-09-01 10:29:33 +0200

    Move customisable parts of pdfs to functiona that can be overridden

--- invoice/conf/settings.py
+++ invoice/conf/settings.py
@@ -2,35 +2,8 @@
 from os import path
 from django.conf import settings
 
-try:
-    INVOICE_LOGO = settings.INVOICE_LOGO
-except AttributeError:
-    INVOICE_LOGO = path.join(settings.MEDIA_ROOT, 'static/images/logo.png')
 
-try:
-    INVOICE_CURRENCY = settings.INVOICE_CURRENCY
-except AttributeError:
-    INVOICE_CURRENCY = 'EUR'
-
-try:
-    INVOICE_CURRENCY_SYMBOL = settings.INVOICE_CURRENCY_SYMBOL
-except AttributeError:
-    INVOICE_CURRENCY_SYMBOL = '€'
-
-try:
-    INVOICE_NOTE = settings.INVOICE_NOTE
-except AttributeError:
-    INVOICE_NOTE = (
-        'Please make all cheques payable to __________________.',
-    )
-
-try:
-    INVOICE_BUSINESS = settings.INVOICE_BUSINESS
-except AttributeError:
-    INVOICE_BUSINESS = (
-        'Your address',
-        '',
-        'and contact',
-        'details goes',
-        'here.'
-    )
+INV_MODULE = getattr(settings, 'INV_MODULE',  'invoice.pdf')
+INV_LOGO = getattr(settings, 'INV_LOGO', path.join(settings.MEDIA_ROOT, 'static/images/logo.jpg'))
+INV_CURRENCY = getattr(settings, 'INV_CURRENCY', u'EUR')
+INV_CURRENCY_SYMBOL = getattr(settings, 'INV_CURRENCY_SYMBOL', u'€')
--- invoice/pdf.py
+++ invoice/pdf.py
@@ -1,35 +1,88 @@
-from reportlab.pdfgen import canvas
+from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Table
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import cm
+
+try:
+    from django.utils import importlib
+except ImportError:
+    import importlib
 
 from invoice.conf import settings
 from invoice.utils import format_currency
 
 
-def draw_pdf(file_object, invoice):
-    # Create canvas
-    cnv = canvas.Canvas(file_object, pagesize=A4)
-    width, height = A4
+def draw_header(canvas):
+    """ Draws the invoice header """
+    canvas.setStrokeColorRGB(0.9, 0.5, 0.2)
+    canvas.setFillColorRGB(0.2, 0.2, 0.2)
+    canvas.setFont('Helvetica', 16)
+    canvas.drawString(18*cm, -1*cm, 'Invoice')
+    canvas.drawInlineImage(settings.INV_LOGO, 1*cm, -1*cm, 250, 16)
+    canvas.setLineWidth(4)
+    canvas.line(0, -1.25*cm, 21.7*cm, -1.25*cm)
 
-    # Set colours
-    cnv.setStrokeColorRGB(0.9, 0.5, 0.2)
-    cnv.setFillColorRGB(0.2, 0.2, 0.2)
+def draw_address(canvas):
+    """ Draws the business address """
+    business_details = (
+        u'COMPANY NAME LTD',
+        u'STREET',
+        u'TOWN',
+        U'COUNTY',
+        U'POSTCODE',
+        U'COUNTRY',
+        u'',
+        u'',
+        u'Phone: +00 (0) 000 000 000',
+        u'Email: example@example.com',
+        u'Website: www.example.com',
+        u'Reg No: 00000000'
+    )
+    canvas.setFont('Helvetica', 9)
+    textobject = canvas.beginText(13*cm, -2.5*cm)
+    for line in business_details:
+        textobject.textLine(line)
+    canvas.drawText(textobject)
 
-    # Title
-    cnv.translate(0.5*cm, height-1*cm)
-    cnv.setFont('Helvetica', 16)
-    cnv.drawString(width-3*cm, 0, 'Invoice')
-    cnv.drawInlineImage(settings.INVOICE_LOGO, 0, 0, 250, 16)
-    cnv.setLineWidth(4)
-    cnv.line(-0.5*cm, -0.25*cm, width, -0.25*cm)
+def draw_footer(canvas):
+    """ Draws the invoice footer """
+    note = (
+        u'Bank Details: Street address, Town, County, POSTCODE',
+        u'Sort Code: 00-00-00 Account No: 00000000 (Quote invoice number).',
+        u'Please pay via bank transfer or cheque. All payments should be made in CURRENCY.',
+        u'Make cheques payable to Company Name Ltd.',
+    )
+    textobject = canvas.beginText(1*cm, -27*cm)
+    for line in note:
+        textobject.textLine(line)
+    canvas.drawText(textobject)
 
-    # Change font
-    cnv.setFont('Helvetica', 10)
-    cnv.translate(0.5*cm, -1.5*cm)
+inv_module = importlib.import_module(settings.INV_MODULE)
+header_func = inv_module.draw_header
+address_func = inv_module.draw_address
+footer_func = inv_module.draw_footer
+
+
+def draw_pdf(buffer, invoice):
+    """ Draws the invoice """
+    canvas = Canvas(buffer, pagesize=A4)
+    canvas.translate(0, 29.7*cm)
+    canvas.setFont('Helvetica', 10)
+
+    canvas.saveState()
+    header_func(canvas)
+    canvas.restoreState()
+
+    canvas.saveState()
+    footer_func(canvas)
+    canvas.restoreState()
+
+    canvas.saveState()
+    address_func(canvas)
+    canvas.restoreState()
 
     # Client address
-    textobject = cnv.beginText()
+    textobject = canvas.beginText(1.5*cm, -2.5*cm)
     if invoice.address.contact_name:
         textobject.textLine(invoice.address.contact_name)
     textobject.textLine(invoice.address.address_one)
@@ -40,25 +93,17 @@ def draw_pdf(file_object, invoice):
         textobject.textLine(invoice.address.county)
     textobject.textLine(invoice.address.postcode)
     textobject.textLine(invoice.address.country.name)
-    cnv.drawText(textobject)
-
-    # Business address
-    textobject = cnv.beginText()
-    textobject.setTextOrigin(width-width/2, 0)
-    for line in settings.INVOICE_BUSINESS:
-        textobject.textLine(line)
-    cnv.drawText(textobject)
+    canvas.drawText(textobject)
 
     # Info
-    cnv.translate(0, -4*cm)
-    textobject = cnv.beginText()
+    textobject = canvas.beginText(1.5*cm, -6.75*cm)
     textobject.textLine(u'Invoice ID: %s' % invoice.invoice_id)
     textobject.textLine(u'Invoice Date: %s' % invoice.invoice_date.strftime('%d %b %Y'))
     textobject.textLine(u'Client: %s' % invoice.user.username)
-    cnv.drawText(textobject)
+    canvas.drawText(textobject)
 
     # Items
-    data=  [[u'Quantity', u'Description', u'Amount', u'Total'],]
+    data = [[u'Quantity', u'Description', u'Amount', u'Total'],]
     for item in invoice.items.all():
         data.append([
             item.quantity,
@@ -67,27 +112,18 @@ def draw_pdf(file_object, invoice):
             format_currency(item.total())
         ])
     data.append([u'', u'', u'Total:', format_currency(invoice.total())])
-    table = Table(data,
-        style=[
-            ('FONT', (0,0), (-1,-1), 'Helvetica'),
-            ('FONTSIZE', (0,0), (-1,-1), 10),
-            ('TEXTCOLOR', (0,0), (-1,-1), (0.2, 0.2, 0.2)),
-            ('GRID', (0,0), (-1,-2), 1, (0.7, 0.7, 0.7)),
-            ('GRID', (-2,-1), (-1,-1), 1, (0.7, 0.7, 0.7)),
-            ('ALIGN', (-2,0), (-1,-1), 'RIGHT'),
-            ('BACKGROUND', (0,0), (-1,0), (0.8, 0.8, 0.8)),
-        ],
-        colWidths=[2*cm, 11*cm, 3*cm, 3*cm]
-    )
-    tw, th = table.wrapOn(cnv, 15*cm, 19*cm)
-    table.drawOn(cnv, 0, -2*cm-th)
+    table = Table(data, colWidths=[2*cm, 11*cm, 3*cm, 3*cm])
+    table.setStyle([
+        ('FONT', (0,0), (-1,-1), 'Helvetica'),
+        ('FONTSIZE', (0,0), (-1,-1), 10),
+        ('TEXTCOLOR', (0,0), (-1,-1), (0.2, 0.2, 0.2)),
+        ('GRID', (0,0), (-1,-2), 1, (0.7, 0.7, 0.7)),
+        ('GRID', (-2,-1), (-1,-1), 1, (0.7, 0.7, 0.7)),
+        ('ALIGN', (-2,0), (-1,-1), 'RIGHT'),
+        ('BACKGROUND', (0,0), (-1,0), (0.8, 0.8, 0.8)),
+    ])
+    table.wrapOn(canvas, 15*cm, 19*cm)
+    table.drawOn(canvas, 1*cm, -10*cm)
 
-    # Instructions
-    cnv.translate(0, -3*cm-th)
-    textobject = cnv.beginText()
-    for line in settings.INVOICE_NOTE:
-        textobject.textLine(line)
-    cnv.drawText(textobject)
-
-    cnv.showPage()
-    cnv.save()
+    canvas.showPage()
+    canvas.save()
