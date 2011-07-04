f7f6d05bdfab789c2fd2ce8352a9daf4aa6d2b80
Simon Luijk <simon@luijk.co.uk>
2011-07-04 12:37:16 +0200

    Change ' to "

--- invoice/utils/__init__.py
+++ invoice/utils/__init__.py
@@ -3,13 +3,13 @@ from invoice.conf import settings
 
 
 def format_currency(amount):
-    return u'%s %.2f %s' % (
+    return u"%s %.2f %s" % (
         settings.INV_CURRENCY_SYMBOL, amount, settings.INV_CURRENCY
     )
 
 
 def pdf_response(draw_funk, file_name, *args, **kwargs):
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="%s"' % file_name
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = "attachment; filename='%s'" % file_name
     draw_funk(response, *args, **kwargs)
     return response
