1533e92761effa71646f38636e0f5a06b611ba03
Simon Luijk <simon@luijk.co.uk>
2011-07-14 12:00:38 +0200

    Correct disposition header

--- invoice/utils/__init__.py
+++ invoice/utils/__init__.py
@@ -16,7 +16,7 @@ def format_currency(amount):
 
 def pdf_response(draw_funk, file_name, *args, **kwargs):
     response = HttpResponse(content_type="application/pdf")
-    response["Content-Disposition"] = "attachment; filename='%s'" % file_name
+    response["Content-Disposition"] = "attachment; filename=\"%s\"" % file_name
     draw_funk(response, *args, **kwargs)
     return response
 
