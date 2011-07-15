747af88d56dc274638f515825405f58b0e59b8d7
Simon Luijk <simon@luijk.co.uk>
2011-07-15 17:31:00 +0200

    Make sure user in logged in for showing invoice

--- invoice/views.py
+++ invoice/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 from invoice.models import Invoice
 from invoice.pdf import draw_pdf
 from invoice.utils import pdf_response
@@ -9,6 +10,7 @@ def pdf_view(request, pk):
     return pdf_response(draw_pdf, invoice.file_name(), invoice)
 
 
+@login_required
 def pdf_user_view(request, invoice_id):
     invoice = get_object_or_404(Invoice, invoice_id=invoice_id, user=request.user)
     return pdf_response(draw_pdf, invoice.file_name(), invoice)
